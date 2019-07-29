3642da92dbecaa5bc8ad01ffa487e2bae1d7d666
ikesh pack <packappsforyou@gmail.com>
2019-07-29 16:33:26 -0400

    Created User model and migrated

---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true 
+    validates :first_name,:last_name, presence: true
+    validates :password_digest, :session_token, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    after_initialize :ensure_session_token
+    
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return nil unless user
+        user.is_password?(password) ? user : nil
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+    
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save
+        self.session_token
+    end    
+end
---
+++ db/migrate/20190729203113_create_user.rb
@@ -0,0 +1,13 @@
+class CreateUser < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false 
+      t.string :first_name, null: false
+      t.string :last_name
+        
+      t.timestamps
+    end
+  end
+end
