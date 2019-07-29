7cd1506bde55fc749c6f1dc42974ade6a082a27b
ikesh pack <packappsforyou@gmail.com>
2019-07-29 16:34:15 -0400

    Created UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,24 @@
 class Api::UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+            render :show
+        else
+            @errors = @user.errors.full_messages
+            render :errors
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
