6ba58579143f8a38c52a7fadfdbc0105caed9b0d
ikesh pack <packappsforyou@gmail.com>
2019-07-30 13:04:59 -0400

    Removed absolute positioning of NavBar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 import NavBar from './nav_bar/nav_bar';
 import { Route } from 'react-router-dom';
 import Arbitrary from './app_container'
+import LandingPage from './landing_page';
 
 const App = () => (
   <div>
     <header>
       <NavBar />
     </header>
+
+    <Route exact path='/' component={LandingPage} />
 
     {/* < Arbitrary path="/login" component={LoginFormContainer}/>
     < Arbitrary path="/signup" component={SignupFormContainer}/> */}
---
+++ frontend/components/landing_page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props =>
+    <div id='landing_page'>
+        <div id='call_to_start_session'>
+            <img
+                src="https://images.unsplash.com/photo-1467139701929-18c0d27a7516?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+                alt="people walking beside the mountain" />
+        </div>
+    </div>
