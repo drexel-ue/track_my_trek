1440a1cb5f35a9eac532fd631fe00cf3e137673c
ikesh pack <packappsforyou@gmail.com>
2019-07-31 16:23:48 -0400

    Added gener column to users table

---
+++ db/migrate/20190731201757_add_gender_column.rb
@@ -0,0 +1,5 @@
+class AddGenderColumn < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :gender, :string, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_203113) do
+ActiveRecord::Schema.define(version: 2019_07_31_201757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_203113) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "gender", null: false
   end
 
 end
