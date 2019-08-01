5d69ad87748bdeee9de5f366e62984e9208d8637
ikesh pack <packappsforyou@gmail.com>
2019-08-01 12:51:41 -0400

    Confirmed rendering of map to page

--- frontend/components/trek/trek_create.jsx
+++ frontend/components/trek/trek_create.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export default class TrekCreate extends React.Component {
+    componentDidMount() {
+        const mapOptions = {
+            center: { lat: 37.7758, lng: -122.435 },
+            zoom: 13
+        };
+
+        this.map = new google.maps.Map(this.mapNode, mapOptions);
+    }
     render() {
-        return <div id='map'>dlkn</div>
+        return <div id='map_container' ref={map => this.mapNode = map}></div>
     }
 }
