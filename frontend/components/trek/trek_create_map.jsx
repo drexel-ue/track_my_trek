c28d2cd2e333cf1744baccbbb9b0474306820ab6
ikesh pack <packappsforyou@gmail.com>
2019-08-01 16:28:37 -0400

    Began coding of marker placement

--- frontend/components/trek/trek_create_map.jsx
+++ frontend/components/trek/trek_create_map.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 
 export default class TrekCreateMap extends React.Component {
@@ -8,7 +9,21 @@ export default class TrekCreateMap extends React.Component {
         };
 
         this.map = new google.maps.Map(this.mapNode, mapOptions);
+
+        this.map.addListener('click', (event) => {
+            placeMarkerAndPanTo(event.latLng, map);
+        });
     }
+
+    placeMarkerAndPanTo(latLng, map) {
+        let marker = new google.maps.Marker({
+            position: latLng,
+            // map: map
+        });
+        marker.setMap(map)
+        this.map.panTo(latLng);
+    }
+
     render() {
         return <div id='map_container' ref={map => this.mapNode = map}></div>
     }
