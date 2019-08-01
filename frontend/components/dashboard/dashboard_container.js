a920a89ca747bb9ab4bf96402f5409918d0de1da
ikesh pack <packappsforyou@gmail.com>
2019-08-01 11:31:44 -0400

    Added base dashboard_container

---
+++ frontend/components/dashboard/dashboard_container.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux'
+import Dashboard from './dashboard'
+
+const msp = state => ({
+
+})
+const mdp = dispatch => ({
+
+})
+
+export default connect(msp, mdp)(Dashboard)
