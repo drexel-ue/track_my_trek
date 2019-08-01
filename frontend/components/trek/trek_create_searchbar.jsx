0abcf40622c9015934371ac3c0421a8e78950c46
ikesh pack <packappsforyou@gmail.com>
2019-08-01 13:47:33 -0400

    Built sidbar direction input skeleton

--- frontend/components/trek/trek_create_searchbar.jsx
+++ frontend/components/trek/trek_create_searchbar.jsx
@@ -3,24 +3,45 @@ import React from 'react'
 export default class TrekCreateSearcbar extends React.Component {
     constructor(props) {
         super(props)
-        this.state = { location: '' }
+        this.state = { location: '', goingTo: '' }
     }
 
-    handleInput(event) {
-        this.setState({ location: event.target.value })
+    handleInput(field) {
+        return event =>
+            this.setState({ location: event.target.value })
     }
 
     render() {
+        const locationInput = field => <input
+            type="text"
+            value={this.state[field]}
+            onChange={this.handleInput(field)}
+            placeholder='wherevs'
+        />
+
         return (
             <div className='search_bar'>
-                <input
-                    type="text"
-                    value={this.state.location}
-                    onChange={this.handleInput}
-                    placeholder='wherevs'
-                />
-                <button className='search_button'>Search</button>
+                {locationInput('location')}
+                <button id='search_button'>Search</button>
                 <button className='expand_button'>X</button>
+                <div id='get_directions_div'>
+                    <div className='hide_div'>
+                        <div>Treking to:</div>
+                        <button>X</button>
+                    </div>
+                    <div className='goint_to_input_row'>
+                        {locationInput('goingTo')}
+                        <button>Get Directions</button>
+                    </div>
+                    <div className='transport_type_options'>
+                        <button>foot</button>
+                        <button>bike</button>
+                        <button>car</button>
+                    </div>
+                </div>
+                <div className='show_get_directions'>
+                    <button>Get Directions</button>
+                </div>
             </div>
         )
     }
