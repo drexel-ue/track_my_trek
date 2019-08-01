3f2c4a53bf0ee9115cd348e3ae9a5bdc7cbca4ba
ikesh pack <packappsforyou@gmail.com>
2019-08-01 18:33:44 -0400

    Added custom map markers

--- frontend/components/trek/trek_create_map.jsx
+++ frontend/components/trek/trek_create_map.jsx
@@ -1,7 +1,12 @@
 
 import React from 'react'
+import bootPrint from '../consts/boot_print'
 
 export default class TrekCreateMap extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { leftStep: true }
+    }
     componentDidMount() {
         const mapOptions = {
             center: { lat: 37.7758, lng: -122.435 },
@@ -16,9 +21,19 @@ export default class TrekCreateMap extends React.Component {
     }
 
     placeMarkerAndPanTo(latLng, map) {
+        let image = {
+            url: this.state.leftStep ?
+                'https://image.flaticon.com/icons/svg/2/2058.svg'
+                : 'https://image.flaticon.com/icons/svg/1/1390.svg',
+            scaledSize: new google.maps.Size(20, 20),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(10, 10)
+        }
+        this.setState({ leftStep: !this.state.leftStep })
         let marker = new google.maps.Marker({
             position: latLng,
-            map: map
+            map: map,
+            icon: image
         });
     }
 
