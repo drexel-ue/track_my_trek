4c7fc8414375ef857b6d027da9f334108f58d245
ikesh pack <packappsforyou@gmail.com>
2019-08-02 16:30:37 -0400

    Fixed relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
     validates :password_digest, :session_token, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
 
-    hash_many :treks
+    has_many :treks
 
     after_initialize :ensure_session_token
     
