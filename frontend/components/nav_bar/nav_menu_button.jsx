efe2cb910d7ea956e904095bf782946a635645b9
ikesh pack <packappsforyou@gmail.com>
2019-08-06 17:29:51 -0400

    Fixed toggling

--- frontend/components/nav_bar/nav_menu_button.jsx
+++ frontend/components/nav_bar/nav_menu_button.jsx
@@ -8,6 +8,7 @@ export default class NavMenuButton extends React.Component {
     toggleContents(toggle) {
         return (event) => {
             event.preventDefault()
+            event.stopPropagation()
             const element = document.getElementById('nav_menu_contents')
             toggle == SHOW ?
                 element.classList.remove('hide') :
@@ -24,22 +25,40 @@ export default class NavMenuButton extends React.Component {
             'Logout'
         ]
 
+        const actions = [
+            () => { },
+            () => { },
+            () => { },
+            () => { },
+            this.props.logout
+        ]
+
         return (
-            <div className='nav_menu'>
+            <div
+                className='nav_menu'
+                onMouseOver={this.toggleContents(SHOW)}
+                onMouseLeave={this.toggleContents(HIDE)}
+            >
                 <div
                     id='nav_menu_button'
-                    onMouseOver={this.toggleContents(SHOW)}
-                    onMouseLeave={this.toggleContents(HIDE)}>
+                >
                     <button
                         className='profile_pic'>
                         pic
                 </button>
-                    <div id='nav_menu_contents' className='hide'>
+                    <div
+                        id='nav_menu_contents'
+                        className='hide'
+                    >
                         <ul>
                             {
                                 options.map((option, index) => (
                                     <li key={index}>
-                                        <button>{option}</button>
+                                        <button
+                                            onClick={actions[index]}
+                                        >
+                                            {option}
+                                        </button>
                                     </li>
                                 ))
                             }
--- frontend/components/nav_bar/nav_menu_button_container.js
+++ frontend/components/nav_bar/nav_menu_button_container.js
@@ -1,8 +1,11 @@
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import NavMenuButton from './nav_menu_button';
+import { logout } from '../../actions/session';
 
 const msp = state => ({})
-const mdp = dispatch => ({})
+const mdp = dispatch => ({
+    logout: () => dispatch(logout())
+})
 
 export default withRouter(connect(msp, mdp)(NavMenuButton))
