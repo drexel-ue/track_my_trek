2a2d7c2d613f4131bd8629ac49dbe1a35d6b667c
ikesh pack <packappsforyou@gmail.com>
2019-08-07 17:28:50 -0400

    Replaced links with divs for styling

--- frontend/components/friends/friends.jsx
+++ frontend/components/friends/friends.jsx
@@ -3,14 +3,30 @@ import { Link } from 'react-router-dom'
 
 export default class Friends extends React.Component {
 
+    pushTo(to) {
+        return event => {
+            event.preventDefault()
+            this.props.history.push(to)
+        }
+    }
+
     persist() {
         return (
             <div>
                 <div className='title'>MY FRIENDS</div>
                 <ul className='nav_row'>
-                    <Link to='#'>MY FRIENDS</Link>
-                    <Link to='friends/find'>FIND FRIENDS</Link>
-                    <Link to='friends/invite'>INVITE FRIENDS</Link>
+                    <div
+                        onClick={this.pushTo('/friends')}>
+                        MY FRIENDS
+                        </div>
+                    <div
+                        onClick={this.pushTo('/friends/find')}>
+                        FIND FRIENDS
+                    </div>
+                    <div
+                        onClick={this.pushTo('/friends/invite')}>
+                        INVITE FRIENDS
+                    </div>
                 </ul>
             </div>
         )
