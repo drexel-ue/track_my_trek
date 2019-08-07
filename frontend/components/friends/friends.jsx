de84199867b9ac7c5efeefe47289238927fc736c
ikesh pack <packappsforyou@gmail.com>
2019-08-07 14:15:11 -0400

    Set width and margin for friends component

--- frontend/components/friends/friends.jsx
+++ frontend/components/friends/friends.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props => {
 
@@ -6,6 +7,11 @@ export default props => {
     return (
         <div className='friends'>
             <div className='title'>MY FRIENDS</div>
+            <ul className='nav_row'>
+                <Link to='#'></Link>
+                <Link to='fri'></Link>
+                <Link></Link>
+            </ul>
         </div>
     )
 }
