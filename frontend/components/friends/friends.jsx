ceb2366553e46e3e7d57d0c394da56545505a897
ikesh pack <packappsforyou@gmail.com>
2019-08-07 17:49:23 -0400

    Near completion of persist styling

--- frontend/components/friends/friends.jsx
+++ frontend/components/friends/friends.jsx
@@ -10,20 +10,32 @@ export default class Friends extends React.Component {
         }
     }
 
+    stlyeSelect(match) {
+        return this.props.location.pathname == match ? {
+            borderTop: '3px solid #2F98D9',
+            height: '48px',
+            color: 'black',
+            backgroundColor: 'white'
+        } : {}
+    }
+
     persist() {
         return (
             <div>
                 <div className='title'>MY FRIENDS</div>
                 <ul className='nav_row'>
                     <div
+                        style={this.stlyeSelect('/friends')}
                         onClick={this.pushTo('/friends')}>
                         MY FRIENDS
                         </div>
                     <div
+                        style={this.stlyeSelect('/friends/find')}
                         onClick={this.pushTo('/friends/find')}>
                         FIND FRIENDS
                     </div>
                     <div
+                        style={this.stlyeSelect('/friends/invite')}
                         onClick={this.pushTo('/friends/invite')}>
                         INVITE FRIENDS
                     </div>
