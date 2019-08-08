56695c02db18e604c01a4b6496a8e8ee79087bfd
ikesh pack <packappsforyou@gmail.com>
2019-08-08 08:00:38 -0400

    Added error message for empty query_string

--- frontend/components/friends/prospective_friends_search.jsx
+++ frontend/components/friends/prospective_friends_search.jsx
@@ -1,12 +1,61 @@
 import React from 'react'
 
 export default class ProspectiveFriendsSreach extends React.Component {
-    search() {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            showError: this.props.showError,
+            query_string: this.props.query_string
+        }
+        this.handleInput = this.handleInput.bind(this)
+        this.performSearch = this.performSearch.bind(this)
+    }
+
+    handleInput(event) {
+        event.preventDefault()
+        this.setState({
+            showError: false,
+            query_string: event.target.value
+        })
+    }
+
+    performSearch(event) {
+        event.preventDefault()
+
+        if (this.state.query_string.length > 0) {
+            this.props.fetchProspectiveFriends(this.props.userId, this.state.query_string)
+        } else {
+            this.setState({ showError: true })
+        }
+    }
+
+    error() {
+        return this.state.showError ? (
+            <div className='empty_query_string'>
+                Please enter a name or email...
+            </div>
+        ) : <div></div>
+    }
+
+    searchBar() {
         return (
             <div className='search'>
                 <div className='friend_count'>
                     FIND FRIENDS TO TREK WITH BY FIRST NAME, LAST NAME, OR EMAIL:
                 </div>
+                <div className='search_bar_row'>
+                    <input
+                        type="text"
+                        className='search_input'
+                        onChange={this.handleInput} />
+                    <button
+                        className='search_button'
+                        onClick={this.performSearch}>
+                        SEARCH
+                    </button>
+                </div>
+                {this.error()}
             </div>
         )
     }
@@ -14,7 +63,7 @@ export default class ProspectiveFriendsSreach extends React.Component {
     render() {
         return (
             <div className='prospective_friends_search'>
-                {this.search()}
+                {this.searchBar()}
             </div>
         )
     }
--- frontend/components/friends/prospective_friends_search_container.js
+++ frontend/components/friends/prospective_friends_search_container.js
@@ -1,7 +1,16 @@
 import { connect } from 'react-redux'
 import ProspectiveFriendsSearch from './prospective_friends_search'
+import { fetchProspectiveFriends } from '../../actions/friends';
 
-const msp = state => ({})
-const mdp = dispatch => ({})
+const msp = ({ session }) => ({
+    userId: session.userId,
+    showError: false,
+    query_string: '',
+    friends: []
+})
+const mdp = dispatch => ({
+    fetchProspectiveFriends: (userId, query_string) =>
+        dispatch(fetchProspectiveFriends(userId, query_string))
+})
 
 export default connect(msp, mdp)(ProspectiveFriendsSearch)
