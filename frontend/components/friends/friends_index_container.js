566a5e3bc7e130b5ca1035798cf8bc22c7034290
ikesh pack <packappsforyou@gmail.com>
2019-08-08 20:00:21 -0400

    Fixed reference

--- frontend/components/friends/friends_index_container.js
+++ frontend/components/friends/friends_index_container.js
@@ -5,15 +5,17 @@ import FriendsIndex from './friends_index'
 
 const msp = ({ session, entities }) => {
     const users = Object.values(entities.users)
-    const accepted = users.filter(user =>
-        (session.friends.accepted.some(request => request.requester_id == user.id)
-            || session.friends.accepted.some(request => request.requestee_id == user.id))
-        && user.id != session.userId
-    )
+    const accepted = users.filter(user => {
+        const accepted = Object.values(session.friends.accepted) || []
+            (accepted.some(request => request.requester_id == user.id)
+                || accepted.some(request => request.requestee_id == user.id))
+            && user.id != session.userId
+    })
     const pendings = users.filter(user => !accepted.includes(user) && user.id != session.userId)
     const requestMap = {}
     pendings.forEach(user => {
-        requestMap[user.id] = session.friends.pending.find(request =>
+        const pending = Object.values(session.friends.pending) || []
+        requestMap[user.id] = pending.find(request =>
             request.requester_id == user.id
             || request.requestee_id == user.id
         )
