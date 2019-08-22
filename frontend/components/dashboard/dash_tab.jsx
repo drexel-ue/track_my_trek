8b40e3e7af920c98e451f9fa99173a2b35aa4c77
ikesh pack <packappsforyou@gmail.com>
2019-08-22 18:39:48 -0400

    Add nav tabs

--- frontend/components/dashboard/dash_tab.jsx
+++ frontend/components/dashboard/dash_tab.jsx
@@ -3,3 +3,4 @@ import React from "react";
 export default ({ title }) => {
   return <div className="dash_tab">{title}</div>;
 };
+
--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -2,11 +2,52 @@ import React from "react";
 import DashTabs from "./dash_tabs";
 
 export default class Dashboard extends React.Component {
-  render() {
+  pushTo(to) {
+    return event => {
+      event.preventDefault();
+      this.props.history.push(to);
+    };
+  }
+
+  persistStlyeSelect(match) {
+    return this.props.location.pathname == match
+      ? {
+          borderTop: "3px solid #2F98D9",
+          height: "48px",
+          color: "black",
+          backgroundColor: "white"
+        }
+      : {};
+  }
+
+  persist() {
     return (
-      <div className="dashboard">
-        <DashTabs />
+      <div>
+        <ul className="nav_row">
+          <div
+            style={this.persistStlyeSelect("/dashboard/activity")}
+            onClick={this.pushTo("/dashboard/activity")}
+          >
+            ACTIVITY FEED
+          </div>
+          <div
+            style={this.persistStlyeSelect("/dashboard/user")}
+            onClick={this.pushTo("/dashboard/user")}
+          >
+            MY DASHBOARD
+          </div>
+          <div
+            style={this.persistStlyeSelect("/dashboard/qs")}
+            onClick={this.pushTo("/dashboard/qs")}
+          >
+            24 / 7
+          </div>
+        </ul>
       </div>
     );
+  }
+
+  render() {
+    return <div className="dashboard">{this.persist()}</div>;
   }
 }
