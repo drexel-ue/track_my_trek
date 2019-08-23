402f61be1bc9be77d9c9a188fc92525152a49259
ikesh pack <packappsforyou@gmail.com>
2019-08-23 12:04:24 -0400

    Tabling

--- frontend/components/dashboard/side_bar_info_card.jsx
+++ frontend/components/dashboard/side_bar_info_card.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export default () => {
   return (
     <div className="side_bar_info_card">
-      <div className="top" />
+      <div className="top">
+        <div className="profile_img">
+          <img alt="profile image" src="" />
+        </div>
+      </div>
       <div className="bottom" />
     </div>
   );
--- frontend/components/dashboard/side_bar_info_card_container.js
+++ frontend/components/dashboard/side_bar_info_card_container.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import SideBarInfoCard from "./side_bar_info_card";
 
-const msp = state => ({});
+const msp = ({ entities }) => ({});
 const mdp = dispatch => ({});
 
 export default withRouter(
