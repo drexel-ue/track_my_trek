1d2e572a93133a192317c54934c7c3b1b87bb9c7
ikesh pack <packappsforyou@gmail.com>
2019-10-10 19:49:35 -0400

    Trim down nav bar links

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,55 +1,54 @@
-import React from 'react'
-import HomeButton from './home_button'
-import LoginButtonContainer from './login_button_container'
-import SignupButtonContainer from './signup_button_container'
-import NavBarItemContainer from './nav_bar_item_container'
-import NavMenuButtonContainer from './nav_menu_button_container'
-import DemoUserSwitchContainer from '../session/demo_user_switch_container'
+import React from "react";
+import HomeButton from "./home_button";
+import LoginButtonContainer from "./login_button_container";
+import SignupButtonContainer from "./signup_button_container";
+import NavBarItemContainer from "./nav_bar_item_container";
+import NavMenuButtonContainer from "./nav_menu_button_container";
+import DemoUserSwitchContainer from "../session/demo_user_switch_container";
 
 export default class NavBar extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    authButtons() {
-        return (
-            this.props.history.location.pathname != '/login' && this.props.history.location.pathname != '/signup' ?
-                <div className='session_buttons'>
-                    <LoginButtonContainer key='login_button' placement='nav' />
-                    <SignupButtonContainer key='signup_button' placement='nav' />
-                </div> : <div></div>
-        )
-    }
+  authButtons() {
+    return this.props.history.location.pathname != "/login" &&
+      this.props.history.location.pathname != "/signup" ? (
+      <div className="session_buttons">
+        <LoginButtonContainer key="login_button" placement="nav" />
+        <SignupButtonContainer key="signup_button" placement="nav" />
+      </div>
+    ) : (
+      <div></div>
+    );
+  }
 
-    render() {
-        const titles = [
-            'TRAINING',
-            'TREKS',
-            'CHALLENGES',
-            'GO MVP',
-            'BLOG',
-            'SHOP'
-        ]
+  render() {
+    const titles = ["CREATE", "TREKS"];
 
-        const style = this.props.location.pathname == '/treks/create' ?
-            {
-                marginBottom: '3px',
-                boxShadow: '0 2px 2px rgba(0, 0, 0, 0.2)'
-            } : {}
+    const style =
+      this.props.location.pathname == "/treks/create"
+        ? {
+            marginBottom: "3px",
+            boxShadow: "0 2px 2px rgba(0, 0, 0, 0.2)"
+          }
+        : {};
 
-
-        return <div id='nav_bar'
-            style={style}>
-            <ul>
-                <HomeButton />
-                {titles.map((title, index) =>
-                    <NavBarItemContainer
-                        title={title}
-                        key={index} />)
-                }
-                {this.props.loggedIn ? <NavMenuButtonContainer /> : this.authButtons()}
-                <DemoUserSwitchContainer />
-            </ul>
-        </div>
-    }
+    return (
+      <div id="nav_bar" style={style}>
+        <ul>
+          <HomeButton />
+          {titles.map((title, index) => (
+            <NavBarItemContainer title={title} key={index} />
+          ))}
+          {this.props.loggedIn ? (
+            <NavMenuButtonContainer />
+          ) : (
+            this.authButtons()
+          )}
+          <DemoUserSwitchContainer />
+        </ul>
+      </div>
+    );
+  }
 }
