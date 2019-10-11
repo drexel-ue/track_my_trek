934caee7e18375aca1e161c8f32577e1211d72a6
ikesh pack <packappsforyou@gmail.com>
2019-10-11 14:07:56 -0400

    Trim links

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -9,53 +9,11 @@ export default class Dashboard extends React.Component {
     };
   }
 
-  persistStlyeSelect(match) {
-    return this.props.location.pathname == match
-      ? {
-          borderTop: "3px solid #2F98D9",
-          height: "48px",
-          color: "black",
-          backgroundColor: "white"
-        }
-      : {};
-  }
-
-  persist() {
-    return (
-      <div>
-        <ul className="nav_row">
-          <div
-            style={this.persistStlyeSelect("/dashboard/activity")}
-            onClick={this.pushTo("/dashboard/activity")}
-          >
-            ACTIVITY FEED
-          </div>
-          <div
-            style={this.persistStlyeSelect("/dashboard/user")}
-            onClick={this.pushTo("/dashboard/user")}
-          >
-            MY DASHBOARD
-          </div>
-          <div
-            style={this.persistStlyeSelect("/dashboard/qs")}
-            onClick={this.pushTo("/dashboard/qs")}
-          >
-            24 / 7
-          </div>
-        </ul>
-      </div>
-    );
-  }
-
   render() {
     return (
       <div className="dashboard">
-        {this.persist()}
         <div className="switch">
           <div className="main_content" />
-          <div className="sidebar">
-            <SideBarInfoCard />
-          </div>
         </div>
       </div>
     );
--- frontend/components/user_nav/user_nav.jsx
+++ frontend/components/user_nav/user_nav.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default class UserNav extends React.Component {
-
-    render() {
-        return this.props.userId && this.props.location.pathname != '/treks/create' ? (
-            <div className='user_nav'>
-                <ul>
-                    <Link to='/treks/create'>Track a Trek</Link>
-                    <Link to='#'>Log Workout</Link>
-                    <Link to='#'>Import Workout</Link>
-                    <Link to='#'>Create a Goal</Link>
-                </ul>
-            </div>
-        ) :
-            <div></div>
-    }
+  render() {
+    return this.props.userId &&
+      this.props.location.pathname != "/treks/create" ? (
+      <div className="user_nav">
+        <ul>
+          <Link to="/treks/create">Track a Trek</Link>
+        </ul>
+      </div>
+    ) : (
+      <div></div>
+    );
+  }
 }
