bb651ec22c464e197a01f9cc94ca1e98e19478ed
ikesh pack <packappsforyou@gmail.com>
2019-10-11 11:51:05 -0400

    Syntactic sugar

--- frontend/components/trek/trek_create_side_bar.jsx
+++ frontend/components/trek/trek_create_side_bar.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
-import TrekCreateSearcbar from './trek_create_searchbar';
-import TrekDetailsContainer from './trek_details_container'
-import TrekDirectionsContainer from './trek_create_directions_container';
+import React from "react";
+import TrekCreateSearcbar from "./trek_create_searchbar";
+import TrekDetailsContainer from "./trek_details_container";
+import TrekDirectionsContainer from "./trek_create_directions_container";
 
-export default props => {
-    return (
-        <div className='create_sidebar'>
-            <div className='side_bar_title'>Choose Trek Location</div>
-            <TrekCreateSearcbar />
-            <TrekDetailsContainer />
-            <TrekDirectionsContainer />
-        </div>
-    )
-}
+export default () => (
+  <div className="create_sidebar">
+    <div className="side_bar_title">Choose Trek Location</div>
+    <TrekCreateSearcbar />
+    <TrekDetailsContainer />
+    <TrekDirectionsContainer />
+  </div>
+);
