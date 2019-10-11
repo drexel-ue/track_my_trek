1a8840115d6456b7c12dd7af7a9534a5b3aa032d
ikesh pack <packappsforyou@gmail.com>
2019-10-11 11:56:19 -0400

    Strip uneened section

--- frontend/components/trek/trek_create_searchbar.jsx
+++ frontend/components/trek/trek_create_searchbar.jsx
@@ -1,55 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default class TrekCreateSearcbar extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = { location: '', goingTo: '' }
-    }
+  constructor(props) {
+    super(props);
+    this.state = { location: "", goingTo: "" };
+  }
 
-    handleInput(field) {
-        return event =>
-            this.setState({ location: event.target.value })
-    }
+  handleInput(event) {
+    event.preventDefault();
+    this.setState({ location: event.target.value });
+  }
 
-    toggleGeDirections(event) {
-        event.preventDefault()
-        event.stopPropagation()
-        document.getElementById('get_directions_div').classList.toggle('show')
-        document.getElementById('search_button').classList.toggle('hidden')
-    }
-
-    render() {
-        const locationInput = field => <input
-            type="text"
-            value={this.state[field]}
-            onChange={this.handleInput(field)}
-            placeholder='wherevs'
+  render() {
+    return (
+      <div className="search_bar">
+        <input
+          type="text"
+          value={this.state.location}
+          onChange={this.handleInput}
+          placeholder="Where'd you Trek?"
         />
-
-        return (
-            <div className='search_bar'>
-                {locationInput('location')}
-                <button id='search_button'>Search</button>
-                <button className='expand_button'>X</button>
-                <div id='get_directions_div' className='hidden'>
-                    <div className='hide_get_directions_div'>
-                        <div>Treking to:</div>
-                        <button onClick={this.toggleGeDirections}>X</button>
-                    </div>
-                    <div className='goint_to_input_row'>
-                        {locationInput('goingTo')}
-                        <button>Get Directions</button>
-                    </div>
-                    <div className='transport_type_options'>
-                        <button>foot</button>
-                        <button>bike</button>
-                        <button>car</button>
-                    </div>
-                </div>
-                <div className='show_get_directions'>
-                    <button onClick={this.toggleGeDirections}>Get Directions</button>
-                </div>
-            </div>
-        )
-    }
+        <button id="search_button">Search</button>
+        <button className="expand_button">X</button>
+        <div className="show_get_directions"></div>
+      </div>
+    );
+  }
 }
