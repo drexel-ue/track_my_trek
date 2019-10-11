c09972a311aeb64e87a7d5e1adb678b52c6ea61a
ikesh pack <packappsforyou@gmail.com>
2019-10-11 12:27:50 -0400

    Restyle directions

--- frontend/components/trek/trek_create_directions.jsx
+++ frontend/components/trek/trek_create_directions.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import TrekStep from './trek_create_step'
+import React from "react";
+import TrekStep from "./trek_create_step";
 
 export default class TrekDirections extends React.Component {
-
-    render() {
-        return (
-            <div className='route_directions'>
-                <button>Route Directions/Notes</button>
-                <ul>
-                    {
-                        Object.values(this.props.steps).map((step, index) => (
-                            <li key={index}>
-                                <TrekStep step={step} />
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="route_directions">
+        <div className="title">Trek Steps</div>
+        <ul>
+          {Object.values(this.props.steps).map((step, index) => (
+            <li key={index}>
+              <TrekStep step={step} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
