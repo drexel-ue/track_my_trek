64f67ea7c97715f4d016ea6f1a9e21d7109967cc
ikesh pack <packappsforyou@gmail.com>
2019-10-11 13:56:34 -0400

    Remove extra options

--- frontend/components/nav_bar/nav_menu_button.jsx
+++ frontend/components/nav_bar/nav_menu_button.jsx
@@ -1,50 +1,31 @@
-import React from 'react'
+import React from "react";
 
 export default class NavMenuButton extends React.Component {
+  render() {
+    const options = ["Logout"];
 
-    render() {
-        const options = [
-            'Friends',
-            'Devices',
-            'Support',
-            'Settings',
-            'Logout'
-        ]
+    const actions = [this.props.logout];
 
-        const actions = [
-            () => this.props.history.push('/friends'),
-            () => { },
-            () => { },
-            () => { },
-            this.props.logout
-        ]
-
-        return (
-            <div className='nav_menu'>
-                <div
-                    id='nav_menu_button'
-                >
-                    <button
-                        className='profile_pic'>
-                        <img src="https://image.flaticon.com/icons/svg/149/149071.svg" alt="" />
-                    </button>
-                    <div
-                        id='nav_menu_contents'
-                        className='hide'
-                    >
-                        <ul>
-                            {
-                                options.map((option, index) => (
-                                    <div key={index}
-                                        onClick={actions[index]}>
-                                        {option}
-                                    </div>
-                                ))
-                            }
-                        </ul>
-                    </div>
+    return (
+      <div className="nav_menu">
+        <div id="nav_menu_button">
+          <button className="profile_pic">
+            <img
+              src="https://image.flaticon.com/icons/svg/149/149071.svg"
+              alt=""
+            />
+          </button>
+          <div id="nav_menu_contents" className="hide">
+            <ul>
+              {options.map((option, index) => (
+                <div key={index} onClick={actions[index]}>
+                  {option}
                 </div>
-            </div>
-        )
-    }
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
