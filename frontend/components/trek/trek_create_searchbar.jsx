f8f505b00994e4ffddd71863276528645ce7e036
ikesh pack <packappsforyou@gmail.com>
2019-10-12 11:24:07 -0400

    Add pan to functionality to map

--- frontend/components/trek/trek_create_searchbar.jsx
+++ frontend/components/trek/trek_create_searchbar.jsx
@@ -3,12 +3,27 @@ import React from "react";
 export default class TrekCreateSearcbar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { location: "", goingTo: "" };
+    this.state = { location: "" };
+    this.handleInput = this.handleInput.bind(this);
+    this.geocode = this.geocode.bind(this);
   }
 
   handleInput(event) {
     event.preventDefault();
     this.setState({ location: event.target.value });
+  }
+
+  geocode(event) {
+    event.preventDefault();
+    if (this.state.location.length > 0)
+      $.ajax({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.location
+          .split(" ")
+          .join("+")}&key=AIzaSyDtBUQe2amSSfBXuvk3eS0nkhtokYvCAas`
+      }).then(({ results }) => {
+        const location = results[0].geometry.location;
+        if (location) this.props.map.panTo(location);
+      });
   }
 
   render() {
@@ -19,7 +34,9 @@ export default class TrekCreateSearcbar extends React.Component {
           onChange={this.handleInput}
           placeholder="Where'd you Trek?"
         />
-        <button id="search_button">Search</button>
+        <button onClick={this.geocode} id="search_button">
+          Search
+        </button>
         <button className="expand_button">X</button>
         <div className="show_get_directions"></div>
       </div>
