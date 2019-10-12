37f57e29beb491f8b0ac5f927320dcbd0bc3770f
ikesh pack <packappsforyou@gmail.com>
2019-10-12 09:33:48 -0400

    Change landing page message

--- frontend/components/landing_page.jsx
+++ frontend/components/landing_page.jsx
@@ -1,38 +1,36 @@
-import React from 'react'
-import SignupButtonContainer from './nav_bar/signup_button_container';
-import LoginButtonContainer from './nav_bar/login_button_container';
-import AppStoreLink from './svgs/download_from_app_store';
+import React from "react";
+import SignupButtonContainer from "./nav_bar/signup_button_container";
+import LoginButtonContainer from "./nav_bar/login_button_container";
+import AppStoreLink from "./svgs/download_from_app_store";
 
 export default props => {
-    const topLine = <div className='top_line'></div>
-    const bottomLine = <div className='bottom_line'></div>
+  const topLine = <div className="top_line"></div>;
+  const bottomLine = <div className="bottom_line"></div>;
 
-    return (
-        <div className='landing_page'>
-            <div className='call_to_start_session'>
-                <img
-                    className='pic'
-                    src='https://images.unsplash.com/photo-1467139701929-18c0d27a7516?ixlib=rb-1.2.1&ixclassName=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
-                    alt="people walking beside the mountain"
-                    width='100%' />
-                <div className='info'>
-                    {topLine}
-                    <div className='keep_every_step'>KEEP EVERY STEP</div>
-                    {bottomLine}
-                    <div className='description'>
-                        Track a Trek of any kind, with the support of your closest friends.
-                    </div>
-                    <SignupButtonContainer placement='call_to_start_session' />
-                    <div className='already_a_member'>
-                        <div>Already a member?</div>
-                        <LoginButtonContainer placement='call_to_start_session' />
-                    </div>
-                    {/* <div className='landing_app_store'>
-                        <AppStoreLink />
-                        <AppStoreLink />
-                    </div> */}
-                </div>
-            </div>
+  return (
+    <div className="landing_page">
+      <div className="call_to_start_session">
+        <img
+          className="pic"
+          src="https://images.unsplash.com/photo-1467139701929-18c0d27a7516?ixlib=rb-1.2.1&ixclassName=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+          alt="people walking beside the mountain"
+          width="100%"
+        />
+        <div className="info">
+          {topLine}
+          <div className="keep_every_step">KEEP EVERY STEP</div>
+          {bottomLine}
+          <div className="description">
+            Track a Trek of any kind. Through the woods, through the city, in a
+            canyon. Log that adventure with us :)
+          </div>
+          <SignupButtonContainer placement="call_to_start_session" />
+          <div className="already_a_member">
+            <div>Already a member?</div>
+            <LoginButtonContainer placement="call_to_start_session" />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
